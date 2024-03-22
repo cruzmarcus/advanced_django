681abd46fea2f42053ad8e3b9c938e50fd39dd69
Marcus Cruz <marcus.v.s.c13@gmail.com>
2024-03-22 11:01:30 -0300

    fix

--- 02_01_b/friendsbook/post/models.py
+++ 02_01_b/friendsbook/post/models.py
@@ -1,12 +1,20 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.db import models
 from django.db.models.deletion import CASCADE
+
+BAD_WORDS = {"java", "waterfall", "enterprise"}
+
+
+def validate_no_bad_words(content):
+    if any(word for word in content if word in BAD_WORDS):
+        raise ValidationError("This post contains bad words")
 
 
 class Post(models.Model):
     author = models.ForeignKey(User, null=True, blank=True, on_delete=CASCADE)
-    content = models.CharField(max_length=140)
-    created_on = models.DateTimeField('date created', auto_now_add=True)
+    content = models.CharField(max_length=140, validators=[])
+    created_on = models.DateTimeField("date created", auto_now_add=True)
 
     def __str__(self):
-        return '{}: {}'.format(self.author, self.content[0:20])
+        return f"{self.author}: {self.content[:20]}"
